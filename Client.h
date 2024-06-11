597f8e569d7ae971bfea99ef0302b153e6122944
VyaraTodorova <147301820+VyaraTodorova@users.noreply.github.com>
2024-06-11 21:28:22 +0300

    Client update

--- Client.h
+++ Client.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "Account.h"
-class Client
+class Client : public Account
 {
 public:
 	unsigned check_avl()const;
+	void whoami() const override;
+	void help() const override;
 private:
 
 };
